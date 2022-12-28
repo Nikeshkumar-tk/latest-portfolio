294ca6709b2ecc6beb910436539d8568cb3628ae
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2022-12-28 11:07:39 +0530

    Hero image fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         
             <div className="h-screen">
                 <img
-                    src="src/assets/hero-2.jpg"
+                    src="https://firebasestorage.googleapis.com/v0/b/miniprojectprofile.appspot.com/o/hero-2.jpg?alt=media&token=0226ca2c-9671-45c2-910c-ecf5a2ff0833"
                     alt=""
                     className="object-cover h-full w-full" />
 
